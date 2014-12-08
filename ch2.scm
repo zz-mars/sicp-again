450c38d4956f0c57ad308c4b9cfce18d17d72198
grant chen <grantchen88@gmail.com>
2014-12-08 23:16:50 +0800

    start polynomials

--- ch2.scm
+++ ch2.scm
@@ -1245,3 +1245,69 @@
 ; change 1: arithmatic operation in install-complex-package
 ;  should be re-written with apply-generic
 ; change 2: implement sine and cosine in integer, rational and real package
+
+; poly
+(define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+		 (add-terms (term-list p1) (term-list p2)))
+      (error "Polys not the same variable -- ADD-POLY"
+	     (list p1 p2))))
+(define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+		 (mul-terms (term-list p1) (term-list p2)))
+      (error "Polys not the same variable -- MUL-POLY"
+	     (list p1 p2))))
+
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+	((empty-termlist? L2) L1)
+	(else 
+	 (let ((t1 (first-term L1))
+	       (t2 (first-term L2)))
+	   (let ((od1 (order t1))
+		 (od2 (order t2)))
+	     (cond ((> od1 od2)
+		    (adjoin-term 
+		     t1 (add-terms (rest-terms L1) L2)))
+		   ((> od2 od1)
+		    (adjoin-term 
+		     t2 (add-terms L1 (rest-terms L2))))
+		   (else
+		    (adjoin-term
+		     (make-term od1 (add (coeff t1) (coeff t2)))
+		     (add-terms (rest-terms L1) (rest-terms L2))))))))))
+
+(define (mul-terms L1 L2)
+  (if (empty-termlist? L1)
+      (the-empty-termlist)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2)
+		 (mul-terms (rest-terms L1) L2))))
+(define (mul-term-by-all-terms t L)
+  (if (empty-termlist? L)
+      (the-empty-termlist)
+      (let ((tl (first-term L)))
+	(adjoin-term
+	 (make-term (+ (order t) (order tl))
+		    (mul (coeff t) (coeff tl)))
+	 (mul-term-by-all-terms t (rest-terms L))))))
+
+(define (install-poly-package)
+  (define (make-poly var term-list)
+    (cons var term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+;  (define (add-poly) )
+;  (define (mul-poly) )
+  (define (tag p)
+    (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2)
+	 (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2)
+	 (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
