41a965386cd7dbb7a165455fd402594b52107e97
grant chen <grantchen88@gmail.com>
2015-03-21 20:49:33 +0800

    delayed eval

--- delay_eval.scm
+++ delay_eval.scm
@@ -22,11 +22,27 @@
 (define (stream-car stream) (car stream))
 (define (stream-cdr stream) (force (cdr stream)))
 
-(define (delay expr) (lambda () expr))
-(define (force delayed-obj) (delayed-obj))
+;(define (force delayed-obj) (delayed-obj))
 
 (define (stream-enumerate-interval low high)
   (if (> low high)
       the-empty-stream
       (cons-stream
        low (stream-enumerate-interval (+ low 1) high))))
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+	((pred (stream-car stream))
+	 (cons-stream (stream-car stream)
+		      (stream-filter pred (stream-cdr stream))))
+	(else
+	 (stream-filter pred (stream-cdr stream)))))
+
+; exercise 3.50
+;(define (stream-map proc . argstreams)
+ ; (if (stream-null? (car argstreams))
+  ;    the-empty-stream
+ ;     (cons-stream
+;      (apply proc (map stream-car argstreams))
+;       (apply stream-map
+;	      (cons proc (map stream-cdr argstreams))))))
+
