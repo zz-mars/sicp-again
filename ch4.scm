48740131add5f40a7adc7ff3bc4c505163b04995
grantchen <grantchen88@gmail.com>
2015-05-16 23:29:53 +0800

    make it run first

--- ch4.scm
+++ ch4.scm
@@ -9,8 +9,8 @@
 										 (lambda-body exp) env))
 		  ((begin? exp) (eval-sequence (begin-actions exp) env))
 		  ((cond? exp) (eval (cond->if exp) env))
-		  ((and? exp) (eval-and exp env))
-		  ((or? exp) (eval-or exp env))
+		 ; ((and? exp) (eval-and exp env))
+		 ; ((or? exp) (eval-or exp env))
 		  ((application? exp)
 		   (apply (eval (operator exp) env)
 				  (list-of-values (operands exp) env)))
@@ -54,9 +54,9 @@
 					  env) 'ok)
 
 (define (self-evaluating? exp)
-	(cond ((number? exp) true)
-		  ((string? exp) true)
-		  (else false)))
+	(cond ((number? exp) #t)
+		  ((string? exp) #t)
+		  (else #f)))
 
 (define (variable? exp) (symbol? exp))
 
@@ -66,8 +66,7 @@
 
 (define (tagged-list? exp tag)
 	(if (pair? exp)
-		(eq? (car exp) tag)
-		false))
+		(eq? (car exp) tag) #f))
 
 (define (assignment? exp) (tagged-list? exp 'set!))
 (define (assignment-variable exp) (cadr exp))
@@ -102,7 +101,7 @@
 
 (define (begin? exp) (tagged-list? exp 'begin))
 (define (begin-actions exp) (cdr exp))
-(define (last-exp? seq) (null? (cdr seq))
+(define (last-exp? seq) (null? (cdr seq)))
 (define (first-exp seq) (car seq))
 (define (rest-exps seq) (cdr seq))
 (define (sequence->exp seq)
@@ -141,57 +140,57 @@
 ; (cond ((assoc 'b '((a 1) (b 2))) => cadr)
 ;		(else false))
 ; cond above eval to 2
-(define (expand-clauses clauses)
-	(if (null? clauses) 'false
-		(let ((first-clause (car clauses))
-			  (rest-clauses (cdr clauses)))
-		 (if (cond-else-clause? first-clause)
-			 (if (null? rest-clauses)
-				 (sequence->exp (cond-actions first-clause))
-				 (error "ELSE clause isn't last -- COND->IF" clauses))
-			 (make-if (cond-predicate first-clause)
-					  (let ((cond-act (cond-actions first-clause)))
-					   (if (eq? (car cond-act) '=>)	; new syntax
-						(list (cadr cond-act) (cond-predicate first-clause))
-					    (sequence->exp (cond-actions first-clause)))) ; old syntax
-					  (expand-clauses rest-clauses))))))
-
-(define (and? exp) (tagged-list? exp 'and))
-(define (make-and and-seq) (cons 'and and-seq))
-(define (eval-and exp env)
-	(let ((and-seq (cdr exp)))
-	 (if (null? and-seq) 'true
-		 (let ((first (car and-seq))
-			   (rest (cdr and-seq)))
-		  (let ((first-res (eval first env)))
-		   (if first-res 
-			   (if (null? rest) first-res (eval-and (make-and rest) env))
-			   first-res))))))
-
-(define (and->if exp)
- (let ((and-seq (cdr exp)))
-  (if (null? and-seq) 'true
-	(let ((first (car and-seq))
-		  (rest (cdr and-seq)))
-	 (make-if first
-	  (if (null? rest) first
-	   (and->if (make-and rest))) 'false)))))
-
-(define (or? exp) (tagged-list? exp 'or))
-(define (make-or or-seq) (cons 'or or-seq))
-(define (eval-or exp env)
-	(let ((or-seq (cdr exp)))
-	 (if (null? or-seq) 'false
-	  (let ((first (car or-seq))
-			(rest (cdr or-seq)))
-	   (let ((first-res (eval first env)))
-		(if first-res first-res 
-			(eval-or (make-or rest))))))))
-
-(define (or->if exp)
- (let ((or-seq (cdr exp)))
-  (if (null? or-seq) 'false
-   (let ((first (car or-seq))
-		 (rest (cdr or-seq)))
-	(make-if first first
-	 (or->if (make-or rest)))))))
+;(define (expand-clauses clauses)
+;	(if (null? clauses) 'false
+;		(let ((first-clause (car clauses))
+;			  (rest-clauses (cdr clauses)))
+;		 (if (cond-else-clause? first-clause)
+;			 (if (null? rest-clauses)
+;				 (sequence->exp (cond-actions first-clause))
+;				 (error "ELSE clause isn't last -- COND->IF" clauses))
+;			 (make-if (cond-predicate first-clause)
+;					  (let ((cond-act (cond-actions first-clause)))
+;					   (if (eq? (car cond-act) '=>)	; new syntax
+;						(list (cadr cond-act) (cond-predicate first-clause))
+;					    (sequence->exp (cond-actions first-clause)))) ; old syntax
+;					  (expand-clauses rest-clauses))))))
+;
+;(define (and? exp) (tagged-list? exp 'and))
+;(define (make-and and-seq) (cons 'and and-seq))
+;(define (eval-and exp env)
+;	(let ((and-seq (cdr exp)))
+;	 (if (null? and-seq) 'true
+;		 (let ((first (car and-seq))
+;			   (rest (cdr and-seq)))
+;		  (let ((first-res (eval first env)))
+;		   (if first-res 
+;			   (if (null? rest) first-res (eval-and (make-and rest) env))
+;			   first-res))))))
+;
+;(define (and->if exp)
+; (let ((and-seq (cdr exp)))
+;  (if (null? and-seq) 'true
+;	(let ((first (car and-seq))
+;		  (rest (cdr and-seq)))
+;	 (make-if first
+;	  (if (null? rest) first
+;	   (and->if (make-and rest))) 'false)))))
+;
+;(define (or? exp) (tagged-list? exp 'or))
+;(define (make-or or-seq) (cons 'or or-seq))
+;(define (eval-or exp env)
+;	(let ((or-seq (cdr exp)))
+;	 (if (null? or-seq) 'false
+;	  (let ((first (car or-seq))
+;			(rest (cdr or-seq)))
+;	   (let ((first-res (eval first env)))
+;		(if first-res first-res 
+;			(eval-or (make-or rest))))))))
+;
+;(define (or->if exp)
+; (let ((or-seq (cdr exp)))
+;  (if (null? or-seq) 'false
+;   (let ((first (car or-seq))
+;		 (rest (cdr or-seq)))
+;	(make-if first first
+;	 (or->if (make-or rest)))))))
