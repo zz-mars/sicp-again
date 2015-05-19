4b7050a0b9c5465d6c7c5792b9e862ff15b1bfb1
grantchen <grantchen88@gmail.com>
2015-05-19 23:55:46 +0800

    seems some bugs in the environment implementation

--- ch4.scm
+++ ch4.scm
@@ -194,3 +194,73 @@
 ;		 (rest (cdr or-seq)))
 ;	(make-if first first
 ;	 (or->if (make-or rest)))))))
+
+; chapter 4.1.3 Evaluator Data Structures
+(define (true? x) (not (eq? x #f)))
+(define (false? x) (eq? x #f))
+
+; for primitive procedure
+(define (apply-primitive-procedure proc args))
+(define (primitive-procedure? proc))
+; for compound procedures
+(define (make-procedure parameters body env)
+ (list 'procedure parameters body env))
+(define (compound-procedure? proc) (tagged-list? proc 'procedure))
+(define (procedure-parameters proc) (cadr proc))
+(define (procedure-body proc) (caddr proc))
+(define (procedure-environment proc) (cadddr proc))
+
+; operation on environments
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+; represent frames
+(define (make-frame vars vals) (cons vars vals))
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+ (set-car! frame (cons var (frame-variables frame)))
+ (set-cdr! frame (cons val (frame-values frame))))
+(define (extend-environment vars vals base-env)
+ (if (= (length vars) (length vals))
+  (cons (make-frame vars vals) base-env)
+  (if (< (length vars) (length vals))
+   (error "Too many arguments supplied" vars vals)
+   (error "Too few arguments supplied" vars vals))))
+(define (lookup-variable-value var env)
+ (define (env-loop env)
+  (define (lookup vars vals)
+   (cond ((null? vars) (env-loop (enclosing-environment env)))
+		 ((eq? var (car vars)) (car vals))
+		 (else (lookup (cdr vars) (cdr vals)))))
+  (if (eq? env the-empty-environment)
+      (error "Unbound variable" var)
+	  (let ((frame (first-frame env)))
+	   (lookup (frame-variables frame)
+			   (frame-values frame)))))
+ (env-loop env))
+
+(define (set-variable-value! var val env)
+ (define (env-loop env)
+  (define (lookup vars vals)
+   (cond ((null? vars) 
+		  (env-loop (enclosing-environment env)))
+		 ((eq? var (car vars)) (set-car! vals val))
+		 (else (lookup (cdr vars) (cdr vals)))))
+  (if (eq? env the-empty-environment)
+   (error "Unbound variable" var)
+   (let ((frame (first-frame env)))
+	(lookup (frame-variables frame)
+			(frame-values frame)))))
+ (env-loop env))
+
+; what if env is '() ?
+; the initial env is '('() '()) ?
+(define (define-variable! var val env)
+ (let ((frame (first-frame env)))
+  (define (lookup vars vals)
+   (cond ((null? vars) (add-binding-to-frame! var val frame))
+		 ((eq? var (car vars)) (set-car! vals val))
+		 (else (lookup (cdr vars) (cdr vals)))))
+  (lookup (frame-variables frame)
+		  (frame-values frame))))
